1c4bb6c85cd0721edc72d53ae281d40251cb083d
Igor dos Reis <igorreis@gmail.com>
2023-09-04 19:38:01 -0300

    refactor: small app.ts and server.ts refactor

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,6 @@ import router from './routes';
 import httpErrorMiddleware from './middlewares/httpError.middleware';
 
 class App {
-  public app: express.Express;
-
   constructor() {
     this.app = express();
     this.app.use(express.json());
@@ -16,7 +14,9 @@ class App {
     this.app.use(httpErrorMiddleware);
   }
 
-  public start(PORT: string | number): void {
+  public app: express.Express;
+
+  public start(PORT: number): void {
     this.app.listen(
       PORT,
       () =>
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config';
-// import app from './app';
 import App from './app';
 
 const PORT = Number(process.env.SERVER_PORT) || 3071;
