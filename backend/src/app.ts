34052b27c19c99e33e3fa5e9735dba038a7ec7a7
Igor dos Reis <igorreis@gmail.com>
2023-09-04 16:40:49 -0300

    refactor: change app.ts to class

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,37 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import router from './routes';
+import httpErrorMiddleware from './middlewares/httpError.middleware';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use('/images', express.static(`${__dirname}/images`));
+// app.use(cors());
+// app.use(express.json());
 
-export default app;
+// export default app;
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+    this.app.use(cors());
+    this.app.use('/images', express.static(`${__dirname}/images`));
+    this.app.use(router);
+    this.app.use(httpErrorMiddleware);
+  }
+
+  public start(PORT: string | number): void {
+    this.app.listen(
+      PORT,
+      () =>
+        // eslint-disable-next-line no-console
+        console.log(`Server is running at http://localhost:${PORT}`),
+      // eslint-disable-next-line function-paren-newline
+    );
+  }
+}
+
+export default App;
