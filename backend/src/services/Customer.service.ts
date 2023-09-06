ee621a6afc4b3f7488f82c1b2d3e955dfa6b0ad0
Igor dos Reis <igorreis@gmail.com>
2023-09-05 22:06:02 -0300

    feat: add getAllSeller service method

--- backend/src/services/Customer.service.ts
+++ backend/src/services/Customer.service.ts
@@ -1,10 +1,21 @@
 import IProduct from '../Interfaces/IProduct';
+import { IUserDb } from '../Interfaces/IUser';
 import ProductModel from '../database/models/ProductModel';
+import UserModel from '../database/models/UserModel';
 
 export default class CustomerService {
   public static async getAllProducts(): Promise<IProduct[]> {
     const allProducts = await ProductModel.findAll();
 
     return allProducts;
+  }
+
+  public static async getAllSellers(): Promise<IUserDb[]> {
+    const allSellers = UserModel.findAll({
+      where: { role: 'seller' },
+      attributes: ['id', 'name'],
+    });
+
+    return allSellers;
   }
 }
