a9396404988f5221209c38f37cd03736d4aa1b84
Igor dos Reis <igorreis@gmail.com>
2023-09-10 19:16:53 -0300

    feat: add checkoutCustomer route

--- backend/src/routes/customer.routes.ts
+++ backend/src/routes/customer.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { CustomerController } from '../controllers';
+import validateTokenMiddleware from '../middlewares/jwt.middleware';
 
 const customerRouter = Router();
 
-customerRouter.get('/products', CustomerController.getAllProducts);
+customerRouter.get('/products', validateTokenMiddleware, CustomerController.getAllProducts);
 
-customerRouter.get('/seller', CustomerController.getAllSellers);
+customerRouter.get('/seller', validateTokenMiddleware, CustomerController.getAllSellers);
+
+customerRouter.post('/checkout', validateTokenMiddleware, CustomerController.checkoutCustomerOder);
 
 export default customerRouter;
