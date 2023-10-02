08c8666891a3dbf32400ddae6d8764ae304e065d
Igor dos Reis <igorreis@gmail.com>
2023-10-02 18:56:50 -0300

    feat: add failed login attempt feature

--- frontend/src/services/handleLoginRequests.ts
+++ frontend/src/services/handleLoginRequests.ts
@@ -10,6 +10,20 @@ const postLogin = async (request: IUserLogin) => {
     const { data, status } = await axios.post<IUser>(loginUrl, request);
     return { userData: data, status };
   } catch (error) {
+    // if (axios.isAxiosError(error)) {
+    //   error.
+    //   const { response: { status } } = error;
+    //   // return response;
+    //   console.log('response: ', response);
+    // }
+    if (axios.isAxiosError<{ error: { data: { message: string } } }>(error)) {
+      // const { response } = error;
+      // console.log('response: ', response?.status);
+      return {
+        userData: { userName: '', email: '', role: '', token: '' },
+        status: 401,
+      };
+    }
     // } catch ({ response: { data, status } }) {
     throw new Error('error msg');
     // return { error };
