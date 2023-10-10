2bc94586bd567a10811b28a3ef5173fee707801f
Igor dos Reis <igorreis@gmail.com>
2023-10-10 17:16:47 -0300

    refactor: remove unnecessary imports

--- frontend/src/pages/customer/products/index.tsx
+++ frontend/src/pages/customer/products/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
